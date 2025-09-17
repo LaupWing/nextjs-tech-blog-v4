5fed0063dbc043296522ec662f4f1d2c21878459
laupwing <laupwing@gmail.com>
2025-09-17 19:34:29 +0200

    Implement code changes to enhance functionality and improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { LoadedContainer } from "@/components/containers/LoadedContainer.client"
 import { IconGithub, IconNewspaper, IconTwitter } from "@/components/Icons"
 import { Accent } from "@/components/ui/accent"
 import { Button } from "@/components/ui/button"
+import { TC } from "@/components/ui/TC"
 import Image from "next/image"
 import Link from "next/link"
 import { FC } from "react"
@@ -197,7 +198,7 @@ const HomeIntro: FC = async () => {
                         ))}
                     </div>
                 </div>
-                {/* <TC className="absolute bottom-0 right-6 transform-gpu w-[calc(100%-3rem)] md:w-[600px] 2xl:w-[900px] h-[calc(100%-3rem)] md:h-[600px] 2xl:h-[900px] opacity-20 dark:opacity-10 stroke-slate-400 dark:stroke-accent" /> */}
+                <TC className="absolute bottom-0 right-6 transform-gpu w-[calc(100%-3rem)] md:w-[600px] 2xl:w-[900px] h-[calc(100%-3rem)] md:h-[600px] 2xl:h-[900px] opacity-20 dark:opacity-10 stroke-slate-400 dark:stroke-accent" />
             </section>
         </LoadedContainer>
     )

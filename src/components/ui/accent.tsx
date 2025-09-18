de1aac96d1fa9eab6e24dbd6d66a01198b6a17dc
laupwing <laupwing@gmail.com>
2025-09-18 16:44:32 +0200

    Fix text color in Accent component for improved visibility

--- src/components/ui/accent.tsx
+++ src/components/ui/accent.tsx
@@ -6,11 +6,11 @@ export const Accent: FC<ComponentPropsWithoutRef<"span">> = ({
     className,
 }) => {
     return (
-        <div className="dark:!bg-gradient-to-r dark:!from-dark dark:!to-dark gradient-animation-slow inline-block text-light py-0.5">
+        <div className="dark:!bg-gradient-to-r dark:!from-dark dark:!to-dark gradient-animation-slow inline-block text-white py-0.5">
             <span
                 className={clsx(
                     className,
-                    "transition-colors dark:gradient-animation-slow dark:bg-clip-text dark:text-transparent"
+                    "transition-colors gradient-animation-slow dark:bg-clip-text dark:text-transparent"
                 )}
             >
                 {children}

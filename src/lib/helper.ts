7dce6b9a6ce9a4a20d9bc644526747834ee25bdc
laupwing <laupwing@gmail.com>
2025-09-18 17:05:16 +0200

    Add Prisma integration and implement attachContentMeta function for content metadata management

---
+++ src/lib/helper.ts
@@ -0,0 +1,39 @@
+import { ContentType, PickFrontmatter } from "@/types/frontmatters"
+import { prisma } from "./prisma"
+
+export const attachContentMeta = async <T extends ContentType>(
+    frontmatters: Array<PickFrontmatter<T>>
+) => {
+    const results = []
+
+    for (const frontmatter of frontmatters) {
+        const data = await prisma.contentMeta.upsert({
+            where: {
+                slug: frontmatter.slug,
+            },
+            update: {
+                slug: frontmatter.slug,
+            },
+            create: {
+                slug: frontmatter.slug,
+            },
+            include: {
+                _count: {
+                    select: {
+                        Like: true,
+                        View: true,
+                    },
+                },
+            },
+        })
+
+        results.push({
+            ...frontmatter,
+            views: data._count.View,
+            likes: data._count.Like,
+        })
+        prisma.$disconnect()
+    }
+
+    return results
+}

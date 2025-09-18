367f4ee40cd3d7f0241ec3367c1e1fa9b67f0362
laupwing <laupwing@gmail.com>
2025-09-18 17:02:23 +0200

    Add gray-matter dependency and implement getAllFilesFrontmatter function to parse frontmatter and calculate reading time

---
+++ src/lib/mdx.ts
@@ -0,0 +1,27 @@
+import { ContentType, PickFrontmatter } from "@/types/frontmatters"
+import { readdirSync, readFileSync } from "fs"
+import matter from "gray-matter"
+import { join } from "path"
+import readingTime from "reading-time"
+
+export async function getAllFilesFrontmatter<T extends ContentType>(type: T) {
+    const files = readdirSync(join(process.cwd(), "src", "contents", type))
+
+    return files.reduce((allPosts: Array<PickFrontmatter<T>>, postSlug) => {
+        const source = readFileSync(
+            join(process.cwd(), "src", "contents", type, postSlug),
+            "utf8"
+        )
+        const { data } = matter(source)
+
+        const res = [
+            {
+                ...(data as PickFrontmatter<T>),
+                slug: postSlug.replace(".mdx", ""),
+                readingTime: readingTime(source),
+            },
+            ...allPosts,
+        ]
+        return res
+    }, [])
+}

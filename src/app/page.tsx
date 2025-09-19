59bcbd1c55bfef43ef55cffc8a33e84cea010bbe
laupwing <laupwing@gmail.com>
2025-09-19 11:33:36 +0200

    Update TC component stroke color in HomeIntro for improved visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ const HomeIntro: FC = async () => {
                         ))}
                     </div>
                 </div>
-                <TC className="absolute bottom-0 right-6 transform-gpu w-[calc(100%-3rem)] md:w-[600px] 2xl:w-[900px] h-[calc(100%-3rem)] md:h-[600px] 2xl:h-[900px] opacity-20 dark:opacity-10 stroke-slate-400 dark:stroke-accent" />
+                <TC className="absolute bottom-0 right-6 transform-gpu w-[calc(100%-3rem)] md:w-[600px] 2xl:w-[900px] h-[calc(100%-3rem)] md:h-[600px] 2xl:h-[900px] opacity-20 dark:opacity-10 stroke-slate-400 dark:stroke-emerald-900" />
             </section>
         </LoadedContainer>
     )

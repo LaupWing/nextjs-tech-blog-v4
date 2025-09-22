d61b3a31ba286264225a87ba0a7837e782adf68e
laupwing <laupwing@gmail.com>
2025-09-22 18:59:18 +0200

    Add dracula.css for syntax highlighting and update layout imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import "./dracula.css"
+// import "./mdx.css"
 import { Header } from "@/components/Header"
 import { Footer } from "@/components/Footer"
 

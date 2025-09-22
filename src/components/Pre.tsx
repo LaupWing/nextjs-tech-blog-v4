a0db1acc246bc70159e8bbfa4f9ff4a1ea9cdb4d
laupwing <laupwing@gmail.com>
2025-09-22 11:52:21 +0200

    Add Pre component for enhanced preformatted text rendering

---
+++ src/components/Pre.tsx
@@ -0,0 +1,17 @@
+import type { ComponentPropsWithoutRef, FC } from "react"
+
+interface PreProps extends ComponentPropsWithoutRef<"pre"> {}
+
+export const Pre: FC<PreProps> = (props) => {
+    return (
+        <pre {...props}>
+            {props.children}
+            <style jsx>{`
+                pre {
+                    position: relative;
+                    padding-top: 2.5rem;
+                }
+            `}</style>
+        </pre>
+    )
+}

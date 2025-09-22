2e578449939fd91598aadf5881965c0987fefbbe
laupwing <laupwing@gmail.com>
2025-09-22 11:07:25 +0200

    Fix import path for CloudinaryImage component in MDXComponents

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,6 +1,5 @@
 import { CustomLink } from "./links/CustomLink"
 import Image from "next/image"
-import { CloudinaryImage } from "./images/CloudinaryImage.client"
 import { CustomCode } from "./sections/CustomCode.client"
 import LiteYouTubeEmbed from "react-lite-youtube-embed"
 import { Pre } from "./sections/Pre"
@@ -9,6 +8,7 @@ import { Split } from "./sections/Split"
 import { TechIcons } from "./TechIcons.client"
 import { TweetCard } from "./cards/TweetCard"
 import { GithubCard } from "./cards/GithubCard.client"
+import { CloudinaryImage } from "./CloudinaryImage.client"
 
 export const MDXComponents = {
     a: CustomLink,

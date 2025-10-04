8cd0a9b053ac3db01387f4503a4fb36b9232a0da
laupwing <laupwing@gmail.com>
2025-10-04 20:23:29 +0200

    Refactor useIncrementViews hook to manage loading state during view incrementing

--- src/hooks/useIncrementViews.tsx
+++ src/hooks/useIncrementViews.tsx
@@ -1,12 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
-const useIncrementViews = (slug: string, runIncrement = false) => {
+export const useIncrementViews = (slug: string, runIncrement = false) => {
+    const [loading, setLoading] = useState(false)
+
     useEffect(() => {
         if (runIncrement) {
+            setLoading(true)
             incrementViews(slug)
+                .then(() => {
+                    setLoading(false)
+                })
+                .catch((e) => {
+                    console.log(e)
+                    setLoading(false)
+                })
         }
     }, [runIncrement, slug])
-    return <div>useIncrementViews</div>
+
+    return {
+        loading,
+    }
 }
 
 async function incrementViews(slug: string) {

8f05fafc11ecb94c915218b2c3d244d62984f23f
laupwing <laupwing@gmail.com>
2025-10-04 20:23:47 +0200

    Remove unnecessary React import in useIncrementViews hook

--- src/hooks/useIncrementViews.tsx
+++ src/hooks/useIncrementViews.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 export const useIncrementViews = (slug: string, runIncrement = false) => {
     const [loading, setLoading] = useState(false)

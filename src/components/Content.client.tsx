5debea968e98d9e7852d75a60b2aae09f23f96ea
laupwing <laupwing@gmail.com>
2025-11-27 09:57:29 +0100

    Refactor Content component to enable loading state and improve rendering logic

--- src/components/Content.client.tsx
+++ src/components/Content.client.tsx
@@ -1,37 +1,37 @@
 "use client"
 import type { FC } from "react"
 
-import { useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { getMDXComponent } from "mdx-bundler/client"
 import { MDXComponents } from "./MDXComponents"
-// import { Loader2 } from "lucide-react"
+import { Loader2 } from "lucide-react"
 
 interface ContentProps {
     code: string
 }
 export const Content: FC<ContentProps> = ({ code }) => {
     const Component = useMemo(() => getMDXComponent(code), [code])
-    // const [loaded, setLoaded] = useState(false)
+    const [loaded, setLoaded] = useState(false)
 
-    // useEffect(() => {
-    //     setLoaded(true)
-    // }, [])
+    useEffect(() => {
+        setLoaded(true)
+    }, [])
 
     return (
         <article className="mdx xl:col-span-1 mt-4 projects prose mx-auto w-full transition-colors dark:prose-invert">
-            <Component
-                components={
-                    {
-                        ...MDXComponents,
-                    } as any
-                }
-            />
-            {/* {!loaded ? (
+            {!loaded ? (
                 <div className="flex items-center justify-center py-8">
                     <Loader2 className="animate-spin h-8" />
                 </div>
             ) : (
-            )} */}
+                <Component
+                    components={
+                        {
+                            ...MDXComponents,
+                        } as any
+                    }
+                />
+            )}
         </article>
     )
 }
